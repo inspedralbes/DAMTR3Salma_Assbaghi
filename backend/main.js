5458f861160c2280243a06315688e8c2ca863e0b
a23salassass <a23salassass@inspedralbes.cat>
2025-03-10 22:36:15 +0100

    docker funciona

--- backend/main.js
+++ backend/main.js
@@ -1,12 +1,23 @@
 const express = require('express');
-const app = express();
+const mysql = require('mysql2');
+const cors = require('cors');
 
-app.use = express();
+const app = express();
+app.use(cors());
+app.use(express.json());
+
 
 app.get('/', (req, res) => {
-  res.send("HOLA");
+  res.send('HOLA');
 });
 
+// app.get('/users', (req, res) => {
+//   db.query('SELECT * FROM users', (err, results) => {
+//     if (err) throw err;
+//     res.json(results);
+//   });
+// });
+
 app.listen(4000, () => {
-  console.log('Server is up!');
+  console.log('localhost:4000');
 });
